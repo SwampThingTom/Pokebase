f8a3332bb8487d3e2c54e1edad5cbf65bb3becb2
Thomas Aylesworth <thomas.aylesworth@gmail.com>
2016-12-10 10:13:05 -0500

    Initial Commit

---
+++ Pokebase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pokebase
+//
+//  Created by Thomas Aylesworth on 12/10/16.
+//  Copyright © 2016 Thomas H Aylesworth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

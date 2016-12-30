7d87998a78b131186831b858c4bf4437cbed18bf
Thomas Aylesworth <thomas.aylesworth@gmail.com>
2016-12-30 18:48:00 -0500

    Filter sequential Pokemon with the same name when importing from CSV

--- Pokebase/PokémonImporter.swift
+++ Pokebase/PokémonImporter.swift
@@ -34,12 +34,81 @@ struct PokémonImporter {
         
         let csvHeaders = ["Name", "Pokémon", "CP", "HP", "Dust Price", "Powered Up?", "Level", "ATK", "DEF", "STA"]
         let csv = CSwiftV(with: csvString, separator: ",", headers: csvHeaders)
+        
+        var previousName = ("", 0)
         csv.keyedRows?.forEach({
-            if let pokémon = pokémonFromDictionary($0) {
-                pokémonArray.append(pokémon)
+            guard var pokémon = pokémonFromDictionary($0) else {
+                return
             }
+            
+            if shouldReplace(name: pokémon.name, previousName: &previousName) {
+                let removed = pokémonArray.popLast()
+                print("Removed \(removed?.name)")
+                pokémon.name = previousName.0
+            }
+            
+            pokémonArray.append(pokémon)
         })
         return pokémonArray
+    }
+    
+    /// Returns true if the previously added pokémon should be replaced by the new pokémon.
+    private static func shouldReplace(name: String?, previousName: inout (String, Int)) -> Bool {
+        guard let name = name else {
+            return false
+        }
+        
+        if let nameAndSequence = nameAndSequence(from: name) {
+            if isSameName(current: nameAndSequence, previous: previousName) {
+                previousName = nameAndSequence
+                return true
+            }
+        }
+        
+        previousName = (name, 0)
+        return false
+    }
+    
+    /// Returns true if the current name matches the previous name and the sequence has advanced by 1.
+    private static func isSameName(current: (String, Int), previous: (String, Int)?) -> Bool {
+        guard let previous = previous else {
+            return false
+        }
+        return current.0 == previous.0 && current.1 == previous.1 + 1
+    }
+    
+    private static func nameAndSequence(from string: Any?) -> (String, Int)? {
+        let nameRegEx = try! NSRegularExpression(pattern: "^(.+) (\\d+)$", options: [])
+        
+        guard let name = string as! String? else {
+            return nil
+        }
+        
+        let nameMatches = nameRegEx.matches(in: name, options: [], range: NSRange(location: 0, length: name.characters.count))
+        guard let nameMatch = nameMatches.first else {
+            return nil
+        }
+        
+        if nameMatch.numberOfRanges != 3 {
+            return nil
+        }
+        
+        return nameAndSequence(from: name, nameRange: nameMatch.rangeAt(1), sequenceRange: nameMatch.rangeAt(2))
+    }
+    
+    private static func nameAndSequence(from string: String, nameRange: NSRange, sequenceRange: NSRange) -> (String, Int)? {
+        let name = substring(of: string, range: nameRange)
+        let sequenceString = substring(of: string, range: sequenceRange)
+        guard let sequence = Int(sequenceString) else {
+            return nil
+        }
+        return (name, sequence)
+    }
+    
+    private static func substring(of string: String, range: NSRange) -> String {
+        let start = string.index(string.startIndex, offsetBy: range.location)
+        let end = string.index(start, offsetBy: range.length)
+        return string.substring(with: start ..< end)
     }
     
     private static func readCsv(file: URL) -> String? {
